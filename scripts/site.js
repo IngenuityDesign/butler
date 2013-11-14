e5b0e932e10b254b46e316dc6566f05fd86416e5
Stephen Parente <sparente@91ferns.com>
2013-11-14 22:16:26 +0000

    Upload: scripts/site.js

--- scripts/site.js
+++ scripts/site.js
@@ -54,7 +54,7 @@ function moveRightOne( sel ) {
 		flooredLef = Math.floor(lef);
 		flooredRight = Math.floor(rightPos);
 		flooredCurLef = Math.floor(parseInt($(sel).css('left')));
-		console.log(Math.abs(flooredLef - flooredRight));
+		console.log(Math.abs(flooredLef - flooredRight)+" for " + $(sel).attr('src'));
 		//after the arithmetic 
 		if (( Math.abs(flooredLef - flooredRight) < window.settings.interval) && !override) {
 			window.PAUSED_TIME = Math.floor(currentTime);
